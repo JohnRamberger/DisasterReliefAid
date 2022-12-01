c3142ad4de672e65887f35af7d5d316b7abb3b44
JohnRamberger <jramberger3@gatech.edu>
2022-12-01 12:29:14 -0500

    Update Register.screen.dart

--- disaster_relief_aid_flutter/lib/Screens/Register.screen.dart
+++ disaster_relief_aid_flutter/lib/Screens/Register.screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import "package:intl/intl.dart";
+
 import '../DRA.config.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -18,7 +20,25 @@ class _RegisterScreenState extends State<RegisterScreen> {
         appBar: AppBar(
           title: const Text("Register"),
         ),
-        body: Container(
+        body: const RegistrationForm());
+  }
+}
+
+class RegistrationForm extends StatefulWidget {
+  const RegistrationForm({super.key});
+
+  @override
+  State<RegistrationForm> createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Padding(
             padding: const EdgeInsets.all(16),
             child: Center(
                 child: Column(
@@ -28,7 +48,48 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   child: Row(
                       children: const [Expanded(child: LanguageDropDown())]),
                 ),
-                const Text("asdadsad")
+                Container(
+                  margin: const EdgeInsets.only(bottom: 32),
+                  child: Row(children: [
+                    Expanded(
+                        child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "First Name",
+                        hintText: "Enter your first name",
+                      ),
+                      autofillHints: const [AutofillHints.givenName],
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ))
+                  ]),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 32),
+                  child:
+                      Row(children: const [Expanded(child: BirthdayPicker())]),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 32),
+                  child: Row(children: const [
+                    Expanded(child: VulnerabilitiesDropdown())
+                  ]),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // process data woo!
+                          }
+                        },
+                        child: const Text("Submit"))
+                  ],
+                )
               ],
             ))));
   }
@@ -46,7 +107,7 @@ class _LanguageDropDownState extends State<LanguageDropDown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
+    return DropdownButtonFormField<String>(
       value: dropdownValue,
       items: Config.languages.map<DropdownMenuItem<String>>((String e) {
         return DropdownMenuItem<String>(value: e, child: Text(e));
@@ -56,6 +117,91 @@ class _LanguageDropDownState extends State<LanguageDropDown> {
           dropdownValue = value!;
         });
       },
+      isExpanded: true,
+      decoration: const InputDecoration(
+          labelText: "Language", hintText: "Select your language"),
+    );
+  }
+}
+
+class BirthdayPicker extends StatefulWidget {
+  const BirthdayPicker({super.key});
+
+  @override
+  State<BirthdayPicker> createState() => _BirthdayPickerState();
+}
+
+class _BirthdayPickerState extends State<BirthdayPicker> {
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dateController.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: dateController,
+      decoration: const InputDecoration(
+        icon: Icon(Icons.calendar_today),
+        labelText: "Birthdate",
+      ),
+      readOnly: true,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Please enter a birthdate";
+        }
+      },
+      autofillHints: const [AutofillHints.birthday],
+      onTap: () async {
+        DateTime? pickedDate = await showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
+          lastDate: DateTime.now(),
+        );
+        if (pickedDate != null) {
+          String formatted = DateFormat("MM/dd/yyyy").format(pickedDate);
+          setState(() {
+            dateController.text = formatted;
+          });
+        } else {
+          print("Date is not selected");
+        }
+      },
+    );
+  }
+}
+
+class VulnerabilitiesDropdown extends StatefulWidget {
+  const VulnerabilitiesDropdown({super.key});
+
+  @override
+  State<VulnerabilitiesDropdown> createState() =>
+      _VulnerabilitiesDropdownState();
+}
+
+class _VulnerabilitiesDropdownState extends State<VulnerabilitiesDropdown> {
+  String dropdownValue = Config.vulnerabilities[0];
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<String>(
+      value: dropdownValue,
+      items: Config.vulnerabilities.map<DropdownMenuItem<String>>((String e) {
+        return DropdownMenuItem<String>(value: e, child: Text(e));
+      }).toList(),
+      onChanged: (String? value) {
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      isExpanded: true,
+      decoration: const InputDecoration(
+          labelText: "Vulnerabilities",
+          hintText: "Select your vulnerabilities"),
     );
   }
 }
