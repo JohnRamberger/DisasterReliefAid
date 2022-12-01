d2623b71df8fe03734a7969d358ef85a29637d67
JohnRamberger <jramberger3@gatech.edu>
2022-12-01 11:34:39 -0500

    Update DRA.config.dart
    added languages

--- disaster_relief_aid_flutter/lib/DRA.config.dart
+++ disaster_relief_aid_flutter/lib/DRA.config.dart
@@ -1,3 +1,7 @@
 class Config {
+  /// The name of the app
   static const String appName = 'Disaster Relief Aid';
+
+  /// The list of languages to choose from when registering
+  static final languages = ['English', 'Hindi', 'Marathi'];
 }
