2d1ff1215f3b13cfb526d2c4ade274ee2c1a02c3
JohnRamberger <jramberger3@gatech.edu>
2022-12-01 11:51:21 -0500

    Update Register.screen.dart

--- disaster_relief_aid_flutter/lib/Screens/Register.screen.dart
+++ disaster_relief_aid_flutter/lib/Screens/Register.screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../DRA.config.dart';
+
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
@@ -13,10 +15,47 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register"),
-      ),
-      body: const Center(child: Text("Register Screen")),
+        appBar: AppBar(
+          title: const Text("Register"),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(16),
+            child: Center(
+                child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 32),
+                  child: Row(
+                      children: const [Expanded(child: LanguageDropDown())]),
+                ),
+                const Text("asdadsad")
+              ],
+            ))));
+  }
+}
+
+class LanguageDropDown extends StatefulWidget {
+  const LanguageDropDown({super.key});
+
+  @override
+  State<LanguageDropDown> createState() => _LanguageDropDownState();
+}
+
+class _LanguageDropDownState extends State<LanguageDropDown> {
+  String dropdownValue = Config.languages[0];
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      items: Config.languages.map<DropdownMenuItem<String>>((String e) {
+        return DropdownMenuItem<String>(value: e, child: Text(e));
+      }).toList(),
+      onChanged: (String? value) {
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
     );
   }
 }
