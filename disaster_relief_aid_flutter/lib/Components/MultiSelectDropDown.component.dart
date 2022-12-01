8163b59eac772b4de7633d78971bde601b7a5211
JohnRamberger <jramberger3@gatech.edu>
2022-12-01 12:50:53 -0500

    Create MultiSelectDropDown.component.dart

---
+++ disaster_relief_aid_flutter/lib/Components/MultiSelectDropDown.component.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class CustomMultiselectDropDown extends StatefulWidget {
+  final Function(List<String>) selectedList;
+  final List<String> listOFStrings;
+  final String? labelText;
+  final String? hintText;
+
+  const CustomMultiselectDropDown(
+      {super.key,
+      required this.selectedList,
+      required this.listOFStrings,
+      this.labelText,
+      this.hintText});
+
+  @override
+  createState() {
+    return _CustomMultiselectDropDownState();
+  }
+}
+
+class _CustomMultiselectDropDownState extends State<CustomMultiselectDropDown> {
+  List<String> listOFSelectedItem = [];
+  String selectedText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    // var size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      // decoration:
+      //     BoxDecoration(border: Border.all(color: PrimeDentalColors.grey1)),
+      child: ExpansionTile(
+        title: Text(widget.labelText ?? "Select"
+            // style: GoogleFonts.poppins(
+            //   textStyle: TextStyle(
+            //     color: PrimeDentalColors.grey,
+            //     fontWeight: FontWeight.w400,
+            //     fontSize: 15.0,
+            //   ),
+            // ),
+            ),
+        children: <Widget>[
+          ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: widget.listOFStrings.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: const EdgeInsets.only(bottom: 8.0),
+                child: _ViewItem(
+                    item: widget.listOFStrings[index],
+                    selected: (val) {
+                      selectedText = val;
+                      if (listOFSelectedItem.contains(val)) {
+                        listOFSelectedItem.remove(val);
+                      } else {
+                        listOFSelectedItem.add(val);
+                      }
+                      widget.selectedList(listOFSelectedItem);
+                      setState(() {});
+                    },
+                    itemSelected: listOFSelectedItem
+                        .contains(widget.listOFStrings[index])),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ViewItem extends StatelessWidget {
+  final String item;
+  final bool itemSelected;
+  final Function(String) selected;
+
+  const _ViewItem(
+      {required this.item, required this.itemSelected, required this.selected});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding:
+          EdgeInsets.only(left: size.width * .032, right: size.width * .098),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 24.0,
+            width: 24.0,
+            child: Checkbox(
+              value: itemSelected,
+              onChanged: (val) {
+                selected(item);
+              },
+              // activeColor: PrimeDentalColors.blue,
+            ),
+          ),
+          SizedBox(
+            width: size.width * .025,
+          ),
+          Text(
+            item,
+            // style: GoogleFonts.poppins(
+            //   textStyle: TextStyle(
+            //     color: PrimeDentalColors.grey,
+            //     fontWeight: FontWeight.w400,
+            //     fontSize: 17.0,
+            //   ),
+            // ),
+          ),
+        ],
+      ),
+    );
+  }
+}
