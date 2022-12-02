1f489ed1f0c8bccc010a8d85e22231ce018c4e0c
JohnRamberger <jramberger3@gatech.edu>
2022-12-02 14:18:00 -0500

    Update widget_test.dart

--- disaster_relief_aid_flutter/test/widget_test.dart
+++ disaster_relief_aid_flutter/test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:disaster_relief_aid_flutter/main.dart';
+import 'package:disaster_relief_aid_flutter/App.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
